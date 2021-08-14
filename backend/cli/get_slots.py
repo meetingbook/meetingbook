14ec017a23fc85f0fade67b6ada72d32f3efaab8
DramaTourG <lema091992@gmail.com>
2021-08-14 20:25:05 +0200

    Fix(func): update functions don't repeat
    **What**
    
    -update get_slots

--- backend/cli/get_slots.py
+++ backend/cli/get_slots.py
@@ -4,8 +4,9 @@ from datetime import timedelta
 from backend.cli.convert_time import local_to_utc, utc_to_local, collapse_intervals
 
 
-def get_intervals_from_db(params_path, params_filter, params_start, params_end):
+def get_and_print_collapse_intervals_from_db(params_path, params_filter, params_start, params_end):
     """получает объект со слотами из базы данных в зависимости от фильтра"""
+    lst_of_intervals = []
     with db.create_connection(params_path) as con:
         cur = con.cursor()
 
@@ -20,36 +21,23 @@ def get_intervals_from_db(params_path, params_filter, params_start, params_end):
                 cur.execute(SELECT_QUERY, [params_start, params_end])
         else:
             cur.execute(SELECT_QUERY, [params_start, params_end])
-        return cur
+        for interval_tuple in cur:
+            assert len(interval_tuple) == 1
+            interval = interval_tuple[0]
+            lst_of_intervals.append(utc_to_local(interval))
+        lst_collapse_intervals = sorted(collapse_intervals(lst_of_intervals))
+        for collapse_interval in lst_collapse_intervals:
+            print(collapse_interval)
+        return lst_collapse_intervals
 
 
 def get_slots(params):
     """выводит отформатированный список слотов за неделю или за день"""
-    lst_of_intervals = []
     if params.week:
         param_start = local_to_utc(params.week)
         param_end = param_start + timedelta(days=7)
-
-        intervals = get_intervals_from_db(params.path, params.filter, param_start, param_end)
-        for interval_tuple in intervals:
-            assert len(interval_tuple) == 1
-            interval = interval_tuple[0]
-            lst_of_intervals.append(utc_to_local(interval))
-        lst_collapse_intervals = sorted(collapse_intervals(lst_of_intervals))
-        for collapse_interval in lst_collapse_intervals:
-            print(collapse_interval)
-        return lst_collapse_intervals
-
-    if params.day:
+        return get_and_print_collapse_intervals_from_db(params.path, params.filter, param_start, param_end)
+    elif params.day:
         param_start = local_to_utc(params.day)
         param_end = param_start + timedelta(days=1)
-
-        intervals = get_intervals_from_db(params.path, params.filter, param_start, param_end)
-        for interval_tuple in intervals:
-            assert len(interval_tuple) == 1
-            interval = interval_tuple[0]
-            lst_of_intervals.append(utc_to_local(interval))
-        lst_collapse_intervals = sorted(collapse_intervals(lst_of_intervals))
-        for collapse_interval in lst_collapse_intervals:
-            print(collapse_interval)
-        return lst_collapse_intervals
+        return get_and_print_collapse_intervals_from_db(params.path, params.filter, param_start, param_end)
