1ef0bae783c5a762216474a3f2713f51b0dc6197
DramaTourG <lema091992@gmail.com>
2021-08-14 20:51:30 +0200

    fix test get_slots

--- backend/tests/test_get_slots.py
+++ backend/tests/test_get_slots.py
@@ -39,13 +39,13 @@ def booking(params_for_booking):
 def test_get_slots_week(create_test_table_with_data_for_get_slots):
     params_for_get = SimpleNamespace(path="db/test_main_db.sqlite",
                                      week="2021-03-02", filter=None, day=None)
-    assert get_slots.get_slots(params_for_get)[0] == '2021-03-02 00:00:00 - 2021-03-09 00:15:00'
+    assert get_slots.get_slots(params_for_get)[0] == '2021-03-02 00:00:00 - 2021-03-09 00:00:00'
 
 
 def test_get_slots_day(create_test_table_with_data_for_get_slots):
     params_for_get = SimpleNamespace(path="db/test_main_db.sqlite",
                                      week=None, filter=None, day="2021-03-02")
-    assert get_slots.get_slots(params_for_get)[0] == '2021-03-02 00:00:00 - 2021-03-03 00:15:00'
+    assert get_slots.get_slots(params_for_get)[0] == '2021-03-02 00:00:00 - 2021-03-03 00:00:00'
 
 
 def test_get_slots_day_free(create_test_table_with_data_for_get_slots):
@@ -57,7 +57,7 @@ def test_get_slots_day_free(create_test_table_with_data_for_get_slots):
 def test_get_slots_day_booking(create_test_table_with_data_for_get_slots):
     params_for_get = SimpleNamespace(path="db/test_main_db.sqlite",
                                      week=None, filter='booking', day="2021-03-02")
-    assert get_slots.get_slots(params_for_get)[0] == '2021-03-02 10:00:00 - 2021-03-03 00:15:00'
+    assert get_slots.get_slots(params_for_get)[0] == '2021-03-02 10:00:00 - 2021-03-03 00:00:00'
 
 
 def test_get_slots_week_free(create_test_table_with_data_for_get_slots):
@@ -65,7 +65,7 @@ def test_get_slots_week_free(create_test_table_with_data_for_get_slots):
                                      week="2021-03-02", filter='free', day=None)
 
     assert get_slots.get_slots(params_for_get)[0] == '2021-03-02 00:00:00 - 2021-03-02 10:00:00'
-    assert get_slots.get_slots(params_for_get)[1] == '2021-03-03 11:15:00 - 2021-03-09 00:15:00'
+    assert get_slots.get_slots(params_for_get)[1] == '2021-03-03 11:15:00 - 2021-03-09 00:00:00'
 
 
 def test_get_slots_week_booking(create_test_table_with_data_for_get_slots):
