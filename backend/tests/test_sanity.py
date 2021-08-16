511111d3decaaba5102ff3ad5b8355e285e0db7c
eduard <you@example.com>
2021-08-16 21:19:44 +0300

    fix(sanity_test): fix sanity_test
    **What**
    
    - fixed date display for `delete_day` if slots booked
    - fix assert result for `delete_all`
    - fixed place where test was run because changed the project structure
    - fix import in `test-tool`
    
    closed #25

--- backend/tests/test_sanity.py
+++ backend/tests/test_sanity.py
@@ -1,15 +1,14 @@
 import subprocess
 import sys
 import os
-
-import pytest
+from cli.db.convert_time import utc_to_local, local_to_utc
 
 PYTHON_BINARY = sys.executable
 
 
 def run_with_args(args):
     return subprocess.check_output(
-        "{} cli/main.py -p db/test_main_db.sqlite {}".format(PYTHON_BINARY, args),
+        "{} cli -p db/test_main_db.sqlite {}".format(PYTHON_BINARY, args),
         shell=True)
 
 
@@ -19,7 +18,10 @@ def run_booking_with_args(args):
         shell=True)
 
 
-@pytest.mark.skip(reason="Will be fixed in the next PR")
+def time_in_utc_then_in_local(x):
+    return utc_to_local(str(local_to_utc(x)))
+
+
 def test_sanity():
     try:
         os.remove('db/test_main_db.sqlite')
@@ -27,20 +29,17 @@ def test_sanity():
         pass
 
     assert run_with_args("add_interval 2021-05-03T10:00 2021-05-10T11:15") == b""
-    assert run_booking_with_args("2021-05-03T10:15 2021-05-03T11:00 testName test@test -t tteesstt") == b""
-    assert run_with_args("get_slots -d 2021-05-03") == b"2021-05-03 10:00:00 - 2021-05-04 00:15:00\r\n"
-    assert run_with_args("get_slots -d 2021-05-03 -f booking") == b"2021-05-03 10:15:00 - 2021-05-03 11:00:00\r\n"
+    assert run_booking_with_args("2021-05-03T10:15 2021-05-03T10:45 testName test@test -t tteesstt") == b""
+    assert run_with_args("get_slots -d 2021-05-03") == b'2021-05-03 10:00:00 - 2021-05-04 00:15:00\r\n'
+    assert run_with_args("get_slots -d 2021-05-03 -f booking") == b"2021-05-03 10:15:00 - 2021-05-03 10:45:00\r\n"
     assert run_with_args(
-        "get_slots -d 2021-05-03 -f free") == b'''2021-05-03 10:00:00 - 2021-05-03 10:15:00\r\n2021-05-03 11:00:00 -
-        2021-05-04 00:15:00\r\n '''
+        "get_slots -d 2021-05-03 -f free") == b'''2021-05-03 10:00:00 - 2021-05-03 10:15:00\r\n2021-05-03 10:45:00 - 2021-05-04 00:15:00\r\n'''
     assert run_with_args("get_slots -w 2021-05-03") == b"2021-05-03 10:00:00 - 2021-05-10 00:15:00\r\n"
-    assert run_with_args("get_slots -w 2021-05-03 -f booking") == b"2021-05-03 10:15:00 - 2021-05-03 11:00:00\r\n"
+    assert run_with_args("get_slots -w 2021-05-03 -f booking") == b"2021-05-03 10:15:00 - 2021-05-03 10:45:00\r\n"
     assert run_with_args(
-        "get_slots -w 2021-05-03 -f free") == b'''2021-05-03 10:00:00 - 2021-05-03 10:15:00\r\n2021-05-03 11:00:00 -
-        2021-05-10 00:15:00\r\n '''
+        "get_slots -w 2021-05-03 -f free") == b'''2021-05-03 10:00:00 - 2021-05-03 10:15:00\r\n2021-05-03 10:45:00 - 2021-05-10 00:15:00\r\n'''
     assert run_with_args("delete_interval 2021-05-03T12:00 2021-05-08T13:15") == b""
     assert run_with_args("delete_day 2021-05-04") == b""
-    assert run_with_args("delete_day 2021-05-03") == b"Can't delete booked interval: ('2021-05-03 07:15:00'," \
-                                                     b")\r\nCan't delete booked interval: ('2021-05-03 07:30:00'," \
-                                                     b")\r\nCan't delete booked interval: ('2021-05-03 07:45:00',)\r\n "
-    assert run_with_args("delete_all") == b""
+    assert run_with_args(
+        "delete_day 2021-05-03") == b"Can't delete booked interval: 2021-05-03 10:15:00\r\nCan't delete booked interval: 2021-05-03 10:30:00\r\n"
+    assert run_with_args("delete_all") == b'All free slots removed\r\n'
