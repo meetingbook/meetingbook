e5067d1ea2bcdc085b85e7ff37c9d1ea13177a07
eduard <you@example.com>
2021-08-17 12:00:12 +0300

    fix asserts in asnity

--- backend/tests/test_sanity.py
+++ backend/tests/test_sanity.py
@@ -25,16 +25,16 @@ def test_sanity():
 
     assert run_with_args("add_interval 2021-05-03T10:00 2021-05-10T11:15") == b""
     assert run_booking_with_args("2021-05-03T10:15 2021-05-03T10:45 testName test@test -t tteesstt") == b""
-    assert run_with_args("get_slots -d 2021-05-03") == b"2021-05-03 10:00:00 - 2021-05-04 00:15:00\r\n"
-    assert run_with_args("get_slots -d 2021-05-03 -f booking") == b"2021-05-03 10:15:00 - 2021-05-03 10:45:00\r\n"
+    assert run_with_args("get_slots -d 2021-05-03") == b"2021-05-03 10:00:00 - 2021-05-04 00:15:00\n"
+    assert run_with_args("get_slots -d 2021-05-03 -f booking") == b"2021-05-03 10:15:00 - 2021-05-03 10:45:00\n"
     assert run_with_args(
-        "get_slots -d 2021-05-03 -f free") == b'''2021-05-03 10:00:00 - 2021-05-03 10:15:00\r\n2021-05-03 10:45:00 - 2021-05-04 00:15:00\r\n'''
-    assert run_with_args("get_slots -w 2021-05-03") == b"2021-05-03 10:00:00 - 2021-05-10 00:15:00\r\n"
-    assert run_with_args("get_slots -w 2021-05-03 -f booking") == b"2021-05-03 10:15:00 - 2021-05-03 10:45:00\r\n"
+        "get_slots -d 2021-05-03 -f free") == b'''2021-05-03 10:00:00 - 2021-05-03 10:15:00\n2021-05-03 10:45:00 - 2021-05-04 00:15:00\n'''
+    assert run_with_args("get_slots -w 2021-05-03") == b"2021-05-03 10:00:00 - 2021-05-10 00:15:00\n"
+    assert run_with_args("get_slots -w 2021-05-03 -f booking") == b"2021-05-03 10:15:00 - 2021-05-03 10:45:00\n"
     assert run_with_args(
-        "get_slots -w 2021-05-03 -f free") == b'''2021-05-03 10:00:00 - 2021-05-03 10:15:00\r\n2021-05-03 10:45:00 - 2021-05-10 00:15:00\r\n'''
+        "get_slots -w 2021-05-03 -f free") == b'''2021-05-03 10:00:00 - 2021-05-03 10:15:00\n2021-05-03 10:45:00 - 2021-05-10 00:15:00\n'''
     assert run_with_args("delete_interval 2021-05-03T12:00 2021-05-08T13:15") == b""
     assert run_with_args("delete_day 2021-05-04") == b""
     assert run_with_args(
-        "delete_day 2021-05-03") == b"Can't delete booked interval: 2021-05-03 10:15:00\r\nCan't delete booked interval: 2021-05-03 10:30:00\r\n"
-    assert run_with_args("delete_all") == b'All free slots removed\r\n'
+        "delete_day 2021-05-03") == b"Can't delete booked interval: 2021-05-03 10:15:00\nCan't delete booked interval: 2021-05-03 10:30:00\n"
+    assert run_with_args("delete_all") == b'All free slots removed\n'
