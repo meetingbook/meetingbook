12ae8612909db5382169fef97f34a2e6bf075543
Maria <leopardilla@protonmail.com>
2021-08-19 00:24:39 +0300

    feat(login): added REST API for login
    **
    What
    **
    
    - server/admin.py added with HTTPBasicAuth
    - server/app.py updated
    - requirements.txt updated
    
    related to #11

---
+++ backend/server/admin.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify, make_response, Blueprint
+from flask_httpauth import HTTPBasicAuth
+
+admin_page = Blueprint('admin_page', __name__)
+auth = HTTPBasicAuth()
+
+
+@admin_page.route("/admin")
+@auth.login_required
+def admin():
+    return "OK"
+
+
+@auth.get_password
+def get_password(username):
+    if username == 'admin':
+        return 'Pyth0n'
+    return None
+
+
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify({'error': 'Unauthorized access'}), 401)
--- backend/server/app.py
+++ backend/server/app.py
@@ -1,12 +1,14 @@
-from flask import Flask, jsonify
-from server.hello_json import msg_hello
+from flask import Flask, jsonify, make_response
+
+from admin import admin_page
 
 app = Flask(__name__)
+app.register_blueprint(admin_page)
 
 
 @app.route("/", methods=["GET"])
 def index():
-    return jsonify(msg_hello)
+    return jsonify({"greeting": "Hello World"})
 
 
 if __name__ == "__main__":
