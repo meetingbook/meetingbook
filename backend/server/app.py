1f36ddd998c557a5f1d43a9c372869755bbe1bee
eduard <you@example.com>
2021-08-19 22:20:15 +0300

    fix(migration): fix path to db
    **What**
    
    - add README
    - fix path to db
    - add flask-migrate, flask-sqlalchemy to requirements
    
    related #10

--- backend/server/app.py
+++ backend/server/app.py
@@ -2,9 +2,12 @@ from flask import Flask, jsonify
 from server.hello_json import msg_hello
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////meetingbook/backend/db/main_db.sqlite'
+basedir = os.path.abspath(os.path.dirname("db/"))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'main_db.sqlite')
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
