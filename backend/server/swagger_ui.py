492c783d513703f02af997221c92ecb403733fba
eduard <you@example.com>
2021-08-30 16:18:44 +0300

    feat(swagger): add swagger ui
    **What**
    
    - added new blueprint(swagger_ui) for flask
    - added "Documentation API" in README
    
    closed #53

---
+++ backend/server/swagger_ui.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_swagger_ui import get_swaggerui_blueprint
+
+
+SWAGGER_URL = '/api/docs'
+API_URL = 'https://raw.githubusercontent.com/meetingbook/meetingbook/main/backend/api/meetingbook.yaml'
+
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL
+)
