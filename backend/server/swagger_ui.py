2de9fff010c2dc40fc6e8bf1577e60bcf4d7797e
eduard <you@example.com>
2021-08-31 13:41:45 +0300

    feat(swagger): local yaml
    **What**
    
    - added launch with local .yaml
    
    related #53

--- backend/server/swagger_ui.py
+++ backend/server/swagger_ui.py
@@ -2,7 +2,7 @@ from flask_swagger_ui import get_swaggerui_blueprint
 
 
 SWAGGER_URL = '/api/docs'
-API_URL = 'https://raw.githubusercontent.com/meetingbook/meetingbook/main/backend/api/meetingbook.yaml'
+API_URL = '/api/meetingbook.yaml'
 
 swaggerui_blueprint = get_swaggerui_blueprint(
     SWAGGER_URL,
