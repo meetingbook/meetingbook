02fde4e3bfa93b86e91d32630601f5d179dfd5c3
DramaTourG <lema091992@gmail.com>
2021-09-08 13:20:59 +0200

    fix(test): separate log psw in test

--- backend/tests/test_func_login_with_db.py
+++ backend/tests/test_func_login_with_db.py
@@ -3,10 +3,11 @@ from tools.work_with_db import get_psw_from_db
 
 
 def test_get_log_psw_from_db():
-    lst_log_psw = ['admin', 'Pyth0n']
+    log = 'admin'
+    psw = 'Pyth0n'
     test_app = create_test_app_with_db()
-    test_admin = test_app.AdminInfo(email=lst_log_psw[0], psw=lst_log_psw[1])
+    test_admin = test_app.AdminInfo(email=log, psw=psw)
     test_app.db.session.add(test_admin)
     test_app.db.session.commit()
-    psw_from_db = get_psw_from_db(test_app, lst_log_psw[0])
-    assert psw_from_db == lst_log_psw[1]
+    psw_from_db = get_psw_from_db(test_app, log)
+    assert psw_from_db == psw
