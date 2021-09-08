1b57ee9ec7808f864a39556711d1e247438dfd35
DramaTourG <lema091992@gmail.com>
2021-09-08 21:09:52 +0200

    fix(import): changed same names

--- backend/tests/test_func_login_with_db.py
+++ backend/tests/test_func_login_with_db.py
@@ -1,14 +1,14 @@
 from tools.create_db_for_tests import create_test_app_with_db
 from tools.work_with_db import get_psw_from_db
-import db.models as db
+import db.models as models
 
 
 def test_get_log_psw_from_db():
     log = 'admin'
     psw = 'Pyth0n'
     create_test_app_with_db()
-    test_admin = db.AdminInfo(email=log, psw=psw)
-    db.db.session.add(test_admin)
-    db.db.session.commit()
-    psw_from_db = get_psw_from_db(db, log)
+    test_admin = models.AdminInfo(email=log, psw=psw)
+    models.db.session.add(test_admin)
+    models.db.session.commit()
+    psw_from_db = get_psw_from_db(models, log)
     assert psw_from_db == psw
