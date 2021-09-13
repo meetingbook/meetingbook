f03da24d2cb12243dad6d0778da1cf51049461de
DramaTourG <lema091992@gmail.com>
2021-09-13 22:32:39 +0200

    feat(test): add Arrange-Act-Assert

--- backend/tests/test_login_page.py
+++ backend/tests/test_login_page.py
@@ -6,13 +6,21 @@ from tools.create_db_for_tests import create_test_app_with_db
 
 
 def test_login_failed():
+
+    # Arrange
     app_for_test = create_test_app_with_db()
     test_app = app_for_test.test_client()
+
+    # Act
     response = test_app.get("/login/")
+
+    # Assert
     assert response.status == '401 UNAUTHORIZED'
 
 
 def test_login_successful():
+
+    # Arrange
     log = 'admin'
     psw = 'Pyth0n'
     prepared_psw = prepare_psw_for_db(psw)
@@ -22,6 +30,11 @@ def test_login_successful():
     models.db.session.commit()
     test_app = app_for_test.test_client()
     valid_credentials = base64.b64encode(f'{log}:{psw}'.encode()).decode('utf-8')
+
+    # Act
     response = test_app.get("/login/", headers={'Authorization': 'Basic ' + valid_credentials})
+
     models.AdminInfo.query.delete()
+
+    # Assert
     assert response.status == '200 OK'
