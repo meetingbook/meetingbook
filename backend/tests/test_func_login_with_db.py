d7c5b5796f97d998629e1005ff213fb22bc8a17c
DramaTourG <lema091992@gmail.com>
2021-09-16 18:25:11 +0200

    feat(reg):add func for reg
    **What**
    
    - add fuction for adding log psw in db
    - add test
    
    related #75

--- backend/tests/test_func_login_with_db.py
+++ backend/tests/test_func_login_with_db.py
@@ -1,14 +1,11 @@
 from tools.create_db_for_tests import create_test_app_with_db
-from tools.work_with_db import get_psw_from_db
-import db.models as models
+from tools.work_with_db import get_psw_from_db, push_email_psw_to_db
 
 
 def test_get_log_psw_from_db():
     log = 'admin'
     psw = 'Pyth0n'
     create_test_app_with_db()
-    test_admin = models.AdminInfo(email=log, psw=psw)
-    models.db.session.add(test_admin)
-    models.db.session.commit()
+    push_email_psw_to_db(log, psw)
     psw_from_db = get_psw_from_db(log)
     assert psw_from_db == psw
--- backend/tools/work_with_db.py
+++ backend/tools/work_with_db.py
@@ -5,3 +5,9 @@ def get_psw_from_db(email):
     log_psw_db = models.AdminInfo.query.filter_by(email=email).first()
     psw_from_db = log_psw_db.psw
     return psw_from_db
+
+
+def push_email_psw_to_db(email, psw):
+    test_admin = models.AdminInfo(email=email, psw=psw)
+    models.db.session.add(test_admin)
+    models.db.session.commit()
