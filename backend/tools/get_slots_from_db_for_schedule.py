6712b5126fe7c3d1295ec895a92bfdc2c77d03b6
eduard <you@example.com>
2021-09-20 15:23:06 +0300

    fix(query): optimizing query
    **What**
    
    - optimizing query
    
    related #52

--- backend/tools/get_slots_from_db_for_schedule.py
+++ backend/tools/get_slots_from_db_for_schedule.py
@@ -1,8 +1,7 @@
 from db.models import Slots, SlotsShema
 from tools.transform_str_to_datetime_and_back import transform_string_to_datetime, transform_datetime_to_string
 from datetime import timedelta
-from tools.query_slots_with_filter import query_slots_with_filter
-from tools.query_slots_no_filter import query_slots_no_filter
+from tools.query_slots import query_slots
 
 
 def get_slots_from_db_for_schedule(date, filter, days):
@@ -10,17 +9,17 @@ def get_slots_from_db_for_schedule(date, filter, days):
     """
     end_interval = transform_datetime_to_string(transform_string_to_datetime(date) + timedelta(days=days))
     if filter is None:
-        list_of_slots = query_slots_no_filter(date, end_interval)
+        list_of_slots = query_slots(date, end_interval)
         slots_shema = SlotsShema(many=True)
         output = slots_shema.dump(list_of_slots)
         return output
     elif filter == "booking":
-        list_of_booking_slots = query_slots_with_filter(date, end_interval, Slots.booking_id)
+        list_of_booking_slots = query_slots(date, end_interval, Slots.booking_id)
         slots_shema = SlotsShema(many=True)
         output = slots_shema.dump(list_of_booking_slots)
         return output
     elif filter == "available":
-        list_of_available_slots = query_slots_with_filter(date, end_interval, Slots.booking_id.is_(None))
+        list_of_available_slots = query_slots(date, end_interval, Slots.booking_id.is_(None))
         slots_shema = SlotsShema(many=True)
         output = slots_shema.dump(list_of_available_slots)
         return output
--- backend/tools/query_slots_no_filter.py
+++
@@ -1,9 +0,0 @@
-from db.models import Slots
-from sqlalchemy import and_, or_
-
-
-def query_slots_no_filter(date, end_interval):
-    list_of_slots = Slots.query.filter(or_(Slots.start_interval.between(date, end_interval),
-                                           Slots.end_interval.between(date, end_interval),
-                                           and_(Slots.start_interval <= date, Slots.end_interval >= end_interval))).all()
-    return list_of_slots
