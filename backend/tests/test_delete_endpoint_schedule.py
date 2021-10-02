cbd9f6c926363e3dbbab915de949e264a39c6b73
eduard <you@example.com>
2021-10-02 22:17:30 +0300

    fix test

--- backend/tests/test_delete_endpoint_schedule.py
+++ backend/tests/test_delete_endpoint_schedule.py
@@ -1,7 +1,9 @@
 import server as app
+from tools.create_db_for_tests import create_test_app_with_db
 
 
 def test_response_delete():
+    create_test_app_with_db()
     with app.app.test_client() as con:
         resp = con.delete('/schedule/interval_id=1')
-    assert resp.status_code == 200
+    assert resp.json == {'detail': 'Delete error', 'status': 500}
