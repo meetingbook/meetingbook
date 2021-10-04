547835c774fb9a19d201618996aa75f7dff231fa
eduard <you@example.com>
2021-10-04 15:28:30 +0300

    feat(schedule_post): work with authentification
    **What**
    
    - added func(in comment) for work with `check authentification`
      (it is commented out because there is no authentication check yet)
    
    related #70

--- backend/server/schedule_post.py
+++ backend/server/schedule_post.py
@@ -4,10 +4,17 @@ from tools.add_slot_from_db_for_schedule_admin import add_slot_from_db_for_sched
 schedule_post = Blueprint('schedule_post', __name__)
 
 
+'''Uncomment here and in the `add_slot_from_db_for_schedule_admin.py` file when the authentication check is added'''
+
+
 @schedule_post.route("/schedule/start=<start>&end=<end>", methods=['POST'])
+# @auth.login_required
 def new_free_slots_for_admin(start, end):
     """ Filter: booked or available.
         Return JSON with <filter> slots.
-    """
+    # """
+    # email_admin = auth.current_user()
+    # add_slot = add_slot_from_db_for_schedule_admin(start, end, email_admin)
     add_slot = add_slot_from_db_for_schedule_admin(start, end)
+
     return add_slot
--- backend/tools/add_slot_from_db_for_schedule_admin.py
+++ backend/tools/add_slot_from_db_for_schedule_admin.py
@@ -2,17 +2,17 @@ import db.models as models
 from db.models import Slots, SlotsShema
 from flask import make_response, jsonify
 
-msg = {
-    'msg': "Don't added slot"
-}
 
-
+# def add_slot_from_db_for_schedule_admin(start, end, email_admin):
 def add_slot_from_db_for_schedule_admin(start, end):
     """Add new free slots in db for admin schedule.
     If the slot is added - return json with this slots.
     If the slot is don't added - return error msg
     """
     try:
+        # query_get_id_admin = AdminInfo.query.with_entities(AdminInfo.id).filter(AdminInfo.email == email_admin)
+        # id_admin = query_get_id_admin[0]["id"]
+        # slots = models.Slots(start_interval=start, end_interval=end, admin_id = id_admin)
         slots = models.Slots(start_interval=start, end_interval=end)
         models.db.session.add(slots)
         models.db.session.commit()
