15c3c68eab30daece1b69aba460cafc583f8c305
eduard <you@example.com>
2021-10-07 14:37:34 +0300

    fix import in test

--- backend/tests/test_post_schedule_endpoint.py
+++ backend/tests/test_post_schedule_endpoint.py
@@ -1,5 +1,5 @@
 import base64
-import server as serv
+import server as app
 from tools.create_db_for_tests import create_test_app_with_db
 from tools.for_db.work_with_admin_info import add_admin
 from tools.func_for_psw import password_hashing
@@ -10,7 +10,7 @@ valid_credentials = base64.b64encode(b'test@test.test:testtest').decode('utf-8')
 
 
 def test_status_401():
-    with serv.app.test_client() as con:
+    with app.app.test_client() as con:
         resp = con.post('/schedule/start=2022-15-13T11:00&end=2022-10-14T13:00')
     assert resp.status_code == 401
 
@@ -18,7 +18,7 @@ def test_status_401():
 def test_response_post_200():
     create_test_app_with_db()
     add_admin(admin_email, password_hashing(admin_psw))
-    with serv.app.test_client() as con:
+    with app.app.test_client() as con:
         resp = con.post('/schedule/start=2022-10-13T11:00&end=2022-10-14T13:00', headers={'Authorization': 'Basic ' + valid_credentials})
     assert resp.status_code == 200
     assert resp.json == [{'booking_id': None,
@@ -28,12 +28,12 @@ def test_response_post_200():
 
 
 def test_response_post_404():
-    with serv.app.test_client() as con:
+    with app.app.test_client() as con:
         resp = con.post('/schedule/star=2022-10-13T11:00&end=2022-15-14T13:00')
     assert resp.status_code == 404
 
 
 def test_status_400():
-    with serv.app.test_client() as con:
+    with app.app.test_client() as con:
         resp = con.post('/schedule/start=2022-15-13T11:00&end=2022-10-14T13:00', headers={'Authorization': 'Basic ' + valid_credentials})
     assert resp.data == b'400 Bad Request'
