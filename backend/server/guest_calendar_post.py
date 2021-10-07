8dda56b827df51beb4e34326024482fe8d9ee383
DramaTourG <lema091992@gmail.com>
2021-10-07 18:17:18 +0200

    feat(table): added table Links

--- backend/server/guest_calendar_post.py
+++ backend/server/guest_calendar_post.py
@@ -5,19 +5,19 @@ from tools.for_db.work_with_booking_info import booking_slot, BookingSlotExcepti
 guest_calendar_post = Blueprint('guest_calendar_post', __name__)
 
 
-def get_admin_id_by_linked_id(linked_id):
+def get_admin_id_by_link_id(link_id):
     try:
-        link = models.Linkes.query.filter_by(linked_id=linked_id).first()
+        link = models.Links.query.filter_by(link_id=link_id).first()
         return link.admin_id
     except Exception:
         return make_response(jsonify({'status': 401, 'detail': 'link id is invalid'}), 401)
 
 
-@guest_calendar_post.route('/calendar/<str:linked_id>/bookings/', method=['POST'])
+@guest_calendar_post.route('/calendar/<str:link_id>/bookings/', methods=['POST'])
 def booking(linked_id):
     try:
         request_body = request.get_json()
-        admin_id = get_admin_id_by_linked_id(linked_id)
+        admin_id = get_admin_id_by_link_id(linked_id)
         booking_slot(request_body['guest_name'], request_body['guest_email'], request_body['topic'] or None,
                      request_body['start'], request_body['end'], admin_id)
     except BookingSlotException:
