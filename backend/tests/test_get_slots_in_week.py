83e51387ea89371b48bb19e97a0f402bc6d5d822
DramaTourG <lema091992@gmail.com>
2021-10-13 00:40:52 +0200

    feat(admin_id): added filter in schedule_get
    **What**
    
    - added function for getting admin_id from auth
    - added function for creating admin and get id in tests
    - added filter in functions for schedule_get
    - fixed all tests

--- backend/tests/test_get_slots_in_week.py
+++ backend/tests/test_get_slots_in_week.py
@@ -1,15 +1,21 @@
-from tools.create_db_for_tests import create_test_app_with_db
+import pytest
+from tools.create_db_for_tests import create_test_app_with_db, get_admin_id_for_test
 from tools.get_slots_from_db_for_schedule import get_slots_from_db_for_schedule
-import db.models as models
+from db import models
 
 
-def test_get_slots_in_week_empty():
+@pytest.fixture(scope='module')
+def create_admin_id():
     create_test_app_with_db()
-    slots = get_slots_from_db_for_schedule("2021-03-02", filter=None, days=7)
+    yield get_admin_id_for_test()
+
+
+def test_get_slots_in_week_empty(create_admin_id):
+    slots = get_slots_from_db_for_schedule(create_admin_id, "2021-03-02", filter=None, days=7)
     assert slots == []
 
 
-def test_get_slots_in_week():
+def test_get_slots_in_week(create_admin_id):
     first_start_interval = "2021-02-03T10:00"
     first_end_interval = "2021-02-03T12:00"
     second_start_interval = "2021-02-03T13:00"
@@ -32,13 +38,14 @@ def test_get_slots_in_week():
         }
     ]
     booking_inf = models.BookingInfo(name=booking_inf_name, email=booking_inf_email)
-    first_slots = models.Slots(start_interval=first_start_interval, end_interval=first_end_interval, booking_id=booking_id)
-    second_slots = models.Slots(start_interval=second_start_interval, end_interval=second_end_interval)
+    first_slots = models.Slots(start_interval=first_start_interval, end_interval=first_end_interval,
+                               booking_id=booking_id, admin_id=create_admin_id)
+    second_slots = models.Slots(start_interval=second_start_interval, end_interval=second_end_interval,
+                                admin_id=create_admin_id)
     models.db.session.add(booking_inf)
     models.db.session.add(first_slots)
     models.db.session.add(second_slots)
     models.db.session.commit()
 
-    booking_slots = get_slots_from_db_for_schedule("2021-02-03", filter=None, days=7)
+    booking_slots = get_slots_from_db_for_schedule(create_admin_id, "2021-02-03", filter=None, days=7)
     assert booking_slots == json
-    models.Slots.query.delete()
--- backend/tools/get_slots_by_filter.py
+++ backend/tools/get_slots_by_filter.py
@@ -1,16 +1,18 @@
+from sqlalchemy import and_
+
 from db.models import Slots, SlotsShema
 
 
-def get_slots_by_filter(filter):
+def get_slots_by_filter(filter, admin_id):
     """Return list of slots with <filter>
     """
     if filter == "booking":
-        booking_slots = Slots.query.filter(Slots.booking_id).all()
+        booking_slots = Slots.query.filter(and_(Slots.booking_id, Slots.admin_id == admin_id)).all()
         slots_shema = SlotsShema(many=True)
         output = slots_shema.dump(booking_slots)
         return output
     elif filter == "available":
-        booking_slots = Slots.query.filter(Slots.booking_id.is_(None)).all()
+        booking_slots = Slots.query.filter(and_(Slots.booking_id.is_(None), Slots.admin_id == admin_id)).all()
         slots_shema = SlotsShema(many=True)
         output = slots_shema.dump(booking_slots)
         return output
