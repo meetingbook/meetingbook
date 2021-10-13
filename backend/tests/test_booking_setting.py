a6e01ed49d024ec976148150de5cf289033cf7a0
cornhubius <shtirlits58@gmail.com>
2021-10-13 20:29:22 +0300

    fix(test)

--- backend/tests/test_booking_setting.py
+++ backend/tests/test_booking_setting.py
@@ -3,7 +3,6 @@ from tools.create_db_for_tests import create_test_app_with_db
 from tools.func_for_psw import password_hashing
 from tools.get_booking_settings import get_booking_settings
 from tools.for_db.work_with_admin_info import add_admin
-from tools.for_db.work_with_booking_settings import add_booking_settings
 
 
 admin_email = 'test@test.test'
@@ -15,8 +14,8 @@ duration = {"msd": "testduration"}
 start_time = {"msg": "teststart_time"}
 json = [
     {
-        "duration": {"msd": "testduration"},
-        "start_time": {"msg": "teststart_time"}
+        'duration': {'allowed_values': '[15]'},
+        'start_time': {'allowed_values': '[0]'}
     }
 ]
 
@@ -24,7 +23,6 @@ json = [
 def test_get_booking_settings_notempty():
     create_test_app_with_db().test_client()
     add_admin(admin_email, admin_psw)
-    add_booking_settings(duration, start_time, 1)
     booking_settings = get_booking_settings(admin_email)
     assert booking_settings == json
 
@@ -32,7 +30,9 @@ def test_get_booking_settings_notempty():
 def test_status_200():
     with create_test_app_with_db().test_client() as con:
         add_admin(admin_email, admin_psw)
-        add_booking_settings(duration, start_time, 1)
         response = con.get('/booking_settings', headers={'Authorization': 'Basic ' + valid_credentials})
     assert response.status == '200 OK'
-    assert response.json == [{'duration': {'msd': 'testduration'}, 'start_time': {'msg': 'teststart_time'}}]
+    assert response.json == [{
+        'duration': {'allowed_values': '[15]'},
+        'start_time': {'allowed_values': '[0]'}
+    }]
