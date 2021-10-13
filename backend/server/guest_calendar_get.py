aff9a039101aef424ec17b74a92a6233d7b7d9fc
DramaTourG <lema091992@gmail.com>
2021-10-13 03:06:50 +0200

    feat(response): added responses like in swagger

--- backend/server/guest_calendar_get.py
+++ backend/server/guest_calendar_get.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from flask import Blueprint, make_response, jsonify
 from db import models
 from tools.get_slots_by_filter import get_slots_by_filter
@@ -9,5 +11,11 @@ guest_calendar_get = Blueprint('guest_calendar_get', __name__)
 def guest_calendar_get(link_id):
     link = models.Links.query.filter_by(link_id=link_id).first()
     if link is None:
-        return make_response(jsonify({'status': 401, 'detail': 'link id is invalid'}), 401)
-    jsonify({'slots': get_slots_by_filter(link.admin_id, 'available')})
+        return make_response(jsonify({'status': 404, 'detail': 'Shareable link not found'}), 404)
+    elif datetime.fromisoformat(link.valid_until) < datetime.utcnow():
+        return make_response(jsonify({'status': 401, 'detail': 'Unauthorized - link has expired'}), 401)
+    return jsonify({
+      "id": link.id,
+      "valid_until": link.valid_until,
+      "slots": get_slots_by_filter(link.admin_id, 'available')
+    })
