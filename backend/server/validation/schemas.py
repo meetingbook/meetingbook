593d699f2a9aa962050ebdb424573eb8d6ad8a87
DramaTourG <lema091992@gmail.com>
2021-10-13 17:36:47 +0200

    feat(validation_schema): addded reg for datetime

--- backend/server/validation/schemas.py
+++ backend/server/validation/schemas.py
@@ -1,4 +1,7 @@
 email_schema = {'type': 'string', "pattern": r"[^@]+@[^@]+\.[^@]"}
+datetime_schema = {'type': 'string', 'format': 'date-time', "pattern":
+                   r'^(-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])'
+                   r'T(2[0-3]|[01][0-9]):([0-5][0-9]):([0-5][0-9])(\.[0-9]+)?(Z|[+-](?:2[0-3]|[01][0-9]):[0-5][0-9])?$'}
 register_schema = {
     'type': 'object',
     'properties': {
@@ -11,11 +14,11 @@ register_schema = {
 guest_calendar_schema = {
     'type': 'object',
     'properties': {
-        'guest_name': {'type': 'string', "maxLength": 20},
+        'guest_name': {'type': 'string', "minLength": 1, "maxLength": 20},
         'guest_email': email_schema,
         'topic': {'type': 'string', "maxLength": 200},
-        'start': {'type': 'string', 'format': 'date-time'},
-        'end': {'type': 'string', 'format': 'date-time'}
+        'start': datetime_schema,
+        'end': datetime_schema
     },
     "required": ['guest_name', 'guest_email', 'start', 'end']
 }
