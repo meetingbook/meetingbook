529a37196a959b89755bf8f5cbeeeefd699a9e3b
DramaTourG <lema091992@gmail.com>
2021-10-15 16:26:02 +0200

    fix(test): added test_admin

--- backend/tests/test_guest_calendars.py
+++ backend/tests/test_guest_calendars.py
@@ -15,14 +15,13 @@ def app_for_test():
 
 start = '2021-10-07T15:00:56.273Z'
 end = '2021-10-07T16:00:56.273Z'
-link_id = '123456789a'
-admin_id = 1
 dt_for_link = DateTime().utc_plus_delta(days=7)
 end_interval = DateTime().utc_plus_delta(days=10)
 
 
-def test_guest_calendar_post(app_for_test):
-    add_admin('my@mail.com', 'Passw0rd')
+def test_guest_calendar_post(app_for_test, test_admin, link_id):
+    test_admin.register_admin()
+    admin_id = test_admin.get_id()
     add_slots(start, end, admin_id)
     add_slots('2020-09-01T15:00:56.273Z', '2020-09-01T16:00:56.273Z', admin_id)
     add_slots(dt_for_link, end_interval, admin_id)
@@ -52,7 +51,7 @@ def test_guest_calendar_post(app_for_test):
     assert res5.status == '400 BAD REQUEST'
 
 
-def test_guest_calendar_get_200(app_for_test):
+def test_guest_calendar_get_200(app_for_test, link_id):
     res = app_for_test.get(f'/calendars/{link_id}')
     assert res.status == '200 OK'
     assert res.json == {'id': 1, 'slots': [{'id': 3, 'start_interval': dt_for_link, 'end_interval': end_interval,
@@ -64,7 +63,7 @@ def test_guest_calendar_get_404(app_for_test):
     assert res.status == '404 NOT FOUND'
 
 
-def test_guest_calendar_get_401(app_for_test):
-    add_link('abc', admin_id, valid_until='2021-10-12T17:34:59.603Z')
+def test_guest_calendar_get_401(app_for_test, test_admin):
+    add_link('abc', test_admin.get_id(), valid_until='2021-10-12T17:34:59.603Z')
     res = app_for_test.get('/calendars/abc')
     assert res.status == '401 UNAUTHORIZED'
