cb2ceeb9bea56b0a738cc37096b759f4fb59be17
NatalliaMil <milkonatallia@gmail.com>
2021-10-15 21:42:59 +0300

    feat:created welcom page
    related #136

--- webapp/src/pages/welcome/index.jsx
+++ webapp/src/pages/welcome/index.jsx
@@ -1,17 +1,46 @@
 import { Box } from '@mui/system';
-import { Link } from 'react-router-dom';
 import { Title } from '../../ui/components/atoms/title';
+import { styled } from '@mui/system';
+import { Button } from '../../ui/components/atoms/button';
+import GlobalStyles from '@mui/material/GlobalStyles';
+import blob from '../../assets/images/pinkblob_for_welcom.svg';
+
+const inputGlobalStyles = (
+  <GlobalStyles
+    styles={{
+      body: {
+        background: `url(${blob}) no-repeat 50% 50%`,
+        backgroundSize: 'cover',
+        width: '50%',
+        margin: '0 auto',
+      },
+    }}
+  />
+);
 
 export const Welcome = () => {
   return (
-    <>
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-evenly',
+        height: '50vh',
+        marginTop: '7em',
+      }}
+    >
+      {inputGlobalStyles}
       <Title>Welcome</Title>
       <Box>
-        <Link to="/login">Login</Link>
+        <Button fullWidth={true} href="/login">
+          Login
+        </Button>
       </Box>
       <Box>
-        <Link to="/signup">Sign up</Link>
+        <Button fullWidth={true} href="/signup">
+          Sign up
+        </Button>
       </Box>
-    </>
+    </Box>
   );
 };
