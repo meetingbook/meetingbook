94956a6428e02d02992708360c09dc8c83f9c7fd
Maria <leopardilla@protonmail.com>
2021-10-20 01:28:29 +0300

    feat(calendar-link: calendar link fixed, tests added
    closes #107

--- backend/server/calendars_post_for_admin.py
+++ backend/server/calendars_post_for_admin.py
@@ -6,13 +6,15 @@ from server.auth import auth
 from server.validation.schemas import calendar_link_schema
 from tools.for_db.work_with_links import add_link, LinkExistsException
 from tools.datetime_convertations import DateTime
+from tools.for_db.work_with_admin_info import get_admin_id
+
 calendars_post = Blueprint('calendars_post', __name__)
 
 
-def get_expiry_date(expiry_date: str) -> date:
+def get_expiry_date(expiry_date: str = None) -> date:
     """Sets expiry date for link_for_calendar"""
-    if expiry_date is None:
-        return DateTime().utc_plus_delta(days=30)
+    if expiry_date is None or expiry_date == "":
+        return DateTime().convert_to_datetime(DateTime().utc_plus_delta(days=30))
     else:
         return DateTime().convert_to_datetime(expiry_date)
 
@@ -26,17 +28,17 @@ def generate_link_id() -> str:
 @expects_json(calendar_link_schema, check_formats=True)
 @auth.login_required
 def add_info_to_links_table():
-    expiry_date = request.json['valid_until'] if request.json else None
-    admin_email = auth.current_user()
+    expiry_date = request.json['valid_until']
+    admin_id = get_admin_id(auth.current_user())
     link_id = generate_link_id()
     valid_until = get_expiry_date(expiry_date)
 
     try:
-        add_link(link_id, valid_until, admin_email)
+        add_link(link_id, admin_id, valid_until)
     except LinkExistsException:
         return make_response(jsonify({
             "status": 409,
             "detail": "Conflict. This link exists in MeetingBook"
         }), 409)
 
-    return jsonify({"id": link_id, "valid_until": DateTime().convert_to_iso(valid_until)})
+    return jsonify({"id": link_id, "valid_until": DateTime(valid_until).convert_to_iso()})
--- backend/tests/test_calendars_post_for_admin.py
+++ backend/tests/test_calendars_post_for_admin.py
@@ -1,5 +1,38 @@
-from server.calendars_post_for_admin import get_expiry_date
+from datetime import datetime, timedelta
+from server.calendars_post_for_admin import get_expiry_date, generate_link_id
+from flask import json
+
+ISO8601 = "%Y-%m-%dT%H:%M:%S%z"
 
 
 def test_expiry_date_30():
-    print(get_expiry_date())
+    now = datetime.utcnow()
+    now += timedelta(days=30)
+    assert get_expiry_date().strftime(ISO8601) == now.strftime(ISO8601)
+
+
+def test_expiry_date_1_day():
+    one_day = "2022-10-20T21:48:47.000Z"
+    assert get_expiry_date(one_day).strftime(ISO8601) == "2022-10-20T21:48:47"
+
+
+def test_generate_calendar_link_without_valid_until(app_for_test, test_admin):
+    test_admin.register_admin()
+    response = app_for_test.post("/calendars",
+                                 headers=test_admin.get_valid_header(),
+                                 data=json.dumps(dict(valid_until="")),
+                                 content_type='application/json')
+    assert response.status == "200 OK"
+    assert response.json["valid_until"] != ""
+    assert response.json["id"] != ""
+
+
+def test_generate_calendar_link_with_valid_until(app_for_test, test_admin):
+    response = app_for_test.post('/calendars',
+                                 headers=test_admin.get_valid_header(),
+                                 data=json.dumps(dict(valid_until="2022-10-16T23:21:25.908Z")),
+                                 content_type="application/json")
+    assert response.status == '200 OK'
+    assert response.json["valid_until"] != ""
+    assert response.json["id"] != ""
+
