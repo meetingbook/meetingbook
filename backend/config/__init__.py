69d5c7daf8a673320d40610a2164fcac794d7881
cornhubius <shtirlits58@gmail.com>
2021-10-22 14:50:31 +0300

    fix(send_email): variables
    **What**
    
    - made through environment variables for flask-mail
    
    related #150

--- backend/config/__init__.py
+++ backend/config/__init__.py
@@ -6,12 +6,11 @@ class Config():
 
 
 class DevelopmentConfig(Config):
-    basedir = os.path.abspath(os.path.dirname(__file__)) + '/../db'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'main_db.sqlite')
-    MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 465
-    MAIL_USERNAME = 'meetingbookemail@gmail.com'
-    MAIL_PASSWORD = 'Meetingb00k'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI', 'sqlite:///../db/main_db.sqlite')
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = os.environ.get('MAIL_PORT')
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     MAIL_USE_TLS = False
     MAIL_USE_SSL = True
 
