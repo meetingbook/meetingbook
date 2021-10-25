8bb63b067fabdc4bf82b4dd519fb5fae32725313
cornhubius <shtirlits58@gmail.com>
2021-10-25 15:42:22 +0300

    feat(fix): func parameters
    related #150

--- backend/tools/emails.py
+++ backend/tools/emails.py
@@ -2,15 +2,19 @@ from flask_mail import Message, Mail
 from tools.for_db.work_with_admin_info import get_admin_email_by_id
 
 
-def send_email(admin_id, start, end, guest_name, guest_email, topic):
+def send_email(admin_id, request_body):
     from server import app
     mail_req = Mail(app)
     email_admin = get_admin_email_by_id(admin_id)
-    msg = Message('Hello, you have a new booking', sender='booking@meetingbook.com', recipients=[email_admin])
+    msg = Message('Hello, you have a new booking',
+                  sender='booking@meetingbook.com',
+                  recipients=[email_admin])
     body = '''Hey, {name} booked time: {start}-{end}. If you want to contact him: {email}.'''.format(
-        name=guest_name, start=start, end=end, email=guest_email)
-    if topic is None:
-        msg.body = body
-    else:
-        msg.body = '''{body}. User left a message for you: {topic} '''.format(body=body, topic=topic)
+        name=request_body['guest_name'],
+        start=request_body['start'],
+        end=request_body['end'],
+        email=request_body['guest_email'])
+    msg.body = body if request_body['topic'] is None else '''{body}. User left a message for you: {topic}'''.format(
+        body=body,
+        topic=request_body['topic'])
     mail_req.send(msg)
