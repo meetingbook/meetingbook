dc9ac1d0918d87426cf82728fd29ff89ceaa13bf
DramaTourG <lema091992@gmail.com>
2021-10-26 14:01:10 +0200

    fix(config): deleted unused configs

--- backend/tools/create_db_for_tests.py
+++ backend/tools/create_db_for_tests.py
@@ -10,8 +10,6 @@ from tools.func_for_psw import password_hashing
 def create_test_app_with_db():
     app = create_app(config=config_settings['test'])
     app.app_context().push()
-    # app.config['TESTING'] = True
-    # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
     db.drop_all()
     db.create_all()
     return app
