9520de91e9ca56d79f47d9f4504932f9291f0229
cornhubius <shtirlits58@gmail.com>
2021-10-26 15:54:40 +0300

    fix(comment) related #150

--- backend/config/__init__.py
+++ backend/config/__init__.py
@@ -7,9 +7,9 @@ class Config():
 
 class DevelopmentConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI', 'sqlite:///../db/main_db.sqlite')
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')  # Server of the service from which the email will be sent to the admin
-    MAIL_PORT = os.environ.get('MAIL_PORT')  # Port of the service from which the email will be sent to the admin
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')  # Your login (email). If you want send email in gmail use gmail login
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')  # SMTP server from which the letter will be sent to the administrator
+    MAIL_PORT = os.environ.get('MAIL_PORT')  # Port of the SMTP server
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')  # Your login (email)
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')  # Your password.
     MAIL_USE_TLS = False
     MAIL_USE_SSL = True
