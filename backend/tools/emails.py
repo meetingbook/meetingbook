d3d7edd906ad8f0d8f3e2cea2bd63e3f0c3a6c0d
cornhubius <shtirlits58@gmail.com>
2021-10-28 21:30:28 +0300

    fix(flake8)

--- backend/tools/emails.py
+++ backend/tools/emails.py
@@ -7,7 +7,8 @@ def send_email(admin_id, request_body, mail, link_id):
     msg = Message('Hello, you have a new booking',
                   sender='booking@meetingbook.com',
                   recipients=[email_admin])
-    body = '''Hey, {name} booked time: {start}-{end}. If you want to contact him: {email}. Canel booking: http://localhost:5000/calendars/{link_id}/bookings/{booking_id} '''.format(
+    body = '''Hey, {name} booked time: {start}-{end}. If you want to contact him: {email}. \
+            Canel booking: http://localhost:5000/calendars/{link_id}/bookings/{booking_id} '''.format(
         name=request_body['guest_name'],
         start=request_body['start'],
         end=request_body['end'],
